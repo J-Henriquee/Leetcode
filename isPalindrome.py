5773a8efbd2c345dfef1293c51c7f03c2f80c8db
J-Henriquee <josehenriquedoespiritosanto82@gmail.com>
2026-09-05 18:18:58 -0300

    feat: Resolvendo questão isPalindrome

---
+++ isPalindrome.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        s = str(x)
+        esq = 0
+        dir = len(s) - 1
+
+        while dir > esq:
+            if s[esq] != s[dir]:
+                return False
+            else:
+                esq += 1
+                dir -= 1
+        return True
+
+            
+        
+        
+        
